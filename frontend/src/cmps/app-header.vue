5ac0074805232026f88d7f13f9f3d719950864c1
Y-Moshe <moshe.nehemiah@gmail.com>
2023-01-19 20:27:58 +0200

    improve logout feedback

--- frontend/src/cmps/app-header.vue
+++ frontend/src/cmps/app-header.vue
@@ -13,7 +13,13 @@
           <router-link to="/admin/dashboard">Dashboard</router-link>
         </li>
         <li>
-          <el-button type="danger" :icon="SwitchButton" @click="handleLogout"></el-button>
+          <el-button
+            :loading="isLoading"
+            :disabled="isLoading"
+            type="danger"
+            :icon="SwitchButton"
+            @click="handleLogout"
+          ></el-button>
         </li>
       </ul>
       <ul class="clean-list flex gap-15" v-else>
@@ -28,32 +34,30 @@
   </header>
 </template>
 
-<script>
+<script lang="ts" setup>
+import { ref, computed } from 'vue'
+import { useStore } from 'vuex'
 import { ElMessage } from 'element-plus'
 import { SwitchButton } from '@element-plus/icons-vue'
-import { authService } from '../services/auth.service'
 
-export default {
-  methods: {
-    handleLogout() {
-      authService.logout()
-        .then(() => {
-          this.$store.commit({ type: 'setUser', user: null })
-          ElMessage.success('Successfully logged out!')
-        })
-        .catch(() => ElMessage.error('Failed to log out!'))
-    }
-  },
-  computed: {
-    user() {
-      return this.$store.getters.user
-    },
-    profileLink() {
-      return '/profile/' + this.user.username
-    },
-    SwitchButton() {
-      return SwitchButton
-    }
+import { actions } from '../store/modules/user.store'
+import { IUser } from '../models'
+
+const store = useStore()
+const isLoading = ref(false)
+
+const user = computed<IUser>(() => store.getters.user)
+const profileLink = computed(() => '/profile/' + user.value.username)
+
+const handleLogout = async () => {
+  try {
+    isLoading.value = true
+    await store.dispatch(actions.logoutUser())
+    ElMessage.success('Successfully logged out!')
+  } catch (err) {
+    ElMessage.error('Failed to logged out!')
+  } finally {
+    isLoading.value = false
   }
 }
 </script>
