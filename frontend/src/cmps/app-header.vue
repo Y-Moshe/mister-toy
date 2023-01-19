88d0e611622bb86b2508aa2b6d88df817559bd14
Y-Moshe <moshe.nehemiah@gmail.com>
2023-01-19 20:32:07 +0200

    add redirect on logout

--- frontend/src/cmps/app-header.vue
+++ frontend/src/cmps/app-header.vue
@@ -36,6 +36,7 @@
 
 <script lang="ts" setup>
 import { ref, computed } from 'vue'
+import { useRouter } from 'vue-router'
 import { useStore } from 'vuex'
 import { ElMessage } from 'element-plus'
 import { SwitchButton } from '@element-plus/icons-vue'
@@ -45,6 +46,7 @@ import { IUser } from '../models'
 
 const store = useStore()
 const isLoading = ref(false)
+const router = useRouter()
 
 const user = computed<IUser>(() => store.getters.user)
 const profileLink = computed(() => '/profile/' + user.value.username)
@@ -54,6 +56,7 @@ const handleLogout = async () => {
     isLoading.value = true
     await store.dispatch(actions.logoutUser())
     ElMessage.success('Successfully logged out!')
+    router.push('/')
   } catch (err) {
     ElMessage.error('Failed to logged out!')
   } finally {
