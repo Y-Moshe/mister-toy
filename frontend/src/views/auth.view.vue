665b6a2a41fe92d3553de11eac55d9eb136ee783
Y-Moshe <moshe.nehemiah@gmail.com>
2023-01-19 19:18:38 +0200

    using composition API, add form validation

--- frontend/src/views/auth.view.vue
+++ frontend/src/views/auth.view.vue
@@ -1,84 +1,143 @@
 <template>
   <section class="auth-view">
     <el-form
+      status-icon
+      ref="ruleFormRef"
       :model="credentials"
+      :rules="rules"
       :disabled="isLoading"
-      @keydown.enter="handleSubmit"
+      @keydown.enter="handleSubmit(ruleFormRef)"
       label-position="left"
-      label-width="100px">
+      label-width="100px"
+    >
       <el-form-item>
-        <h1>{{ formType }}</h1>
+        <h1>{{ authType }}</h1>
       </el-form-item>
 
-      <el-form-item label="Fullname" v-if="!isLogin" required>
+      <el-form-item label="Fullname" v-if="!isLogin" prop="fullname" required>
         <el-input type="text" v-model="credentials.fullname" />
       </el-form-item>
 
-      <el-form-item label="Username" required>
+      <el-form-item label="Username" prop="username" required>
         <el-input type="text" v-model="credentials.username" v-focus />
       </el-form-item>
-      
-      <el-form-item label="Password" required>
+
+      <el-form-item label="Password" prop="password" required>
         <el-input type="password" v-model="credentials.password" />
       </el-form-item>
 
       <el-form-item>
-        <el-button type="primary" @click="handleSubmit" :loading="isLoading">{{ formType }}</el-button>
+        <el-button
+          type="primary"
+          @click="handleSubmit(ruleFormRef)"
+          :loading="isLoading"
+        >
+          {{ authType }}
+        </el-button>
         <template v-if="isLogin">
           <el-divider>Or</el-divider>
           <el-button type="info" @click="demoLogin" plain>Demo login</el-button>
-          <el-button type="warning" @click="loginAsAdmin" plain>Login as Admin</el-button>
+          <el-button type="warning" @click="loginAsAdmin" plain>
+            Login as Admin
+          </el-button>
         </template>
       </el-form-item>
     </el-form>
   </section>
 </template>
 
-<script>
+<script lang="ts" setup>
+import { reactive, ref, computed } from 'vue'
+import { useRouter } from 'vue-router'
+import { useStore } from 'vuex'
+import { ElMessage, FormInstance, FormRules } from 'element-plus'
+
 import { authService } from '../services/auth.service.js'
 
-export default {
-  props: { isLogin: Boolean },
-  data() {
-    return {
-      credentials: {
-        username: '',
-        password: '',
-        fullname: ''
-      },
-      isLoading: false
-    }
-  },
-  methods: {
-    handleSubmit() {
-      this.isLoading = true
-      const authAction = this.isLogin
-        ? authService.login
-        : authService.signup
+const props = defineProps({ isLogin: Boolean })
 
-      authAction(this.credentials)
-       .then(user => {
-          this.$store.commit({ type: 'setUser', user })
-          this.$router.push('/')
-       })
-       .catch(err => console.log('err', err))
-       .finally(() => this.isLoading = false)
+const credentials = reactive({
+  username: '',
+  password: '',
+  fullname: '',
+})
+const isLoading = ref(false)
+const ruleFormRef = ref<FormInstance>(null)
+const authType = computed(() => (props.isLogin ? 'Login' : 'Signup'))
+const store = useStore()
+const router = useRouter()
+
+const rules = reactive<FormRules>({
+  username: [
+    {
+      required: true,
+      message: 'Username is required!',
+      trigger: 'blur',
     },
-    demoLogin() {
-      this.credentials.username = 'Maor123'
-      this.credentials.password = '123'
-      this.handleSubmit()
+    {
+      min: 3,
+      message: 'Require minimum 3 characters!',
+      trigger: 'blur',
     },
-    loginAsAdmin() {
-      this.credentials.username = 'admin'
-      this.credentials.password = 'admin'
-      this.handleSubmit()
+  ],
+  password: [
+    {
+      required: true,
+      message: 'Password is required!',
+      trigger: 'blur',
     },
-  },
-  computed: {
-    formType() {
-      return this.isLogin ? 'Login' : 'Signup'
-    }
+    {
+      min: 3,
+      message: 'Require minimum 3 characters!',
+      trigger: 'blur',
+    },
+    {
+      max: 16,
+      message: 'Require maximum 16 characters!',
+      trigger: 'blur',
+    },
+  ],
+  fullname: [
+    {
+      required: !props.isLogin,
+      message: 'Fullname is required!',
+      trigger: 'blur',
+    },
+    {
+      min: 3,
+      message: 'Require minimum 3 characters!',
+      trigger: 'blur',
+    },
+  ],
+})
+
+const handleSubmit = async (elForm: FormInstance | undefined) => {
+  const isValid = await elForm.validate((isValid) => isValid)
+  if (!isValid) return ElMessage.error('invalid')
+
+  isLoading.value = true
+  const authAction = props.isLogin ? authService.login : authService.signup
+
+  try {
+    const user = await authAction(credentials)
+    store.commit({ type: 'setUser', user })
+    router.push('/')
+  } catch (err) {
+    console.log('err', err)
+  } finally {
+    isLoading.value = false
   }
+}
+
+const demoLogin = () => {
+  credentials.username = 'Maor123'
+  credentials.password = '123'
+  handleSubmit(ruleFormRef.value)
+}
+
+const loginAsAdmin = () => {
+  credentials.username = 'admin'
+  credentials.password = 'admin'
+  handleSubmit(ruleFormRef.value)
 }
 </script>
