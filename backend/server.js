ccbfd6932d078ccbb1b083511f3c337a407d5b0b
Y-Moshe <moshe.nehemiah@gmail.com>
2023-01-19 17:15:38 +0200

    return index.html on 404

--- backend/server.js
+++ backend/server.js
@@ -50,10 +50,10 @@ app.use('/api/toy',  toyRoutes)
 app.use('/api/review',  reviewRoutes)
 
 // Make every server-side-route to match the index.html
-// so when requesting http://localhost:3030/index.html/car/123 it will still respond with
+// So, when requesting http://localhost:3030/index.html/car/123 it will still respond with
 // our SPA (single page app) (the index.html file) and allow vue-router to take it from there
-// app.get('/**', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'public', 'index.html'))
-// })
+app.get('/**', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
 
 http.listen(PORT, () => console.log(`Server ready at port: ${PORT}!`))
