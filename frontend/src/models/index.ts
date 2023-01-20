357b122896611a9c0e6bb832ad43543dda9d1ca4
Y-Moshe <moshe.nehemiah@gmail.com>
2023-01-20 03:09:37 +0200

    remove ;

--- frontend/src/models/index.ts
+++ frontend/src/models/index.ts
@@ -1,33 +1,33 @@
 export interface IUser {
-  _id?: string;
-  fullname: string;
-  username: string;
-  password?: string;
-  isAdmin: boolean;
+  _id?: string
+  fullname: string
+  username: string
+  password?: string
+  isAdmin: boolean
 }
 
 export interface IToy {
-  _id?: string;
-  name: string;
-  price: number;
-  inStock: boolean;
-  tags: string[];
-  imgUrl: string;
-  reviews: IReview[];
+  _id?: string
+  name: string
+  price: number
+  inStock: boolean
+  tags: string[]
+  imgUrl: string
+  reviews: IReview[]
 }
 
 export interface IReview {
-  _id?: string;
-  userId: string;
-  toyId: string;
-  txt: string;
+  _id?: string
+  userId: string
+  toyId: string
+  txt: string
 }
 
 export interface IFilterBy {
-  keyword: string;
-  page?: number;
-  itemsPerPage?: number;
-  status: '' | 'In-stock' | 'Out-of-Stock';
-  tags: string[];
-  sortBy: 'Name' | 'Price';
+  keyword: string
+  page?: number
+  itemsPerPage?: number
+  status: '' | 'In-stock' | 'Out-of-Stock'
+  tags: string[]
+  sortBy: 'Name' | 'Price'
 }
